fc146aad42a93bfc39f35d3b6d7140a55e3ccfc4
timothyk0908 <timothyk0908@gmail.com>
2015-03-17 09:08:38 +0100

    ça marche !!!

--- Foodlidays-Client/ProduitsTableVC.swift
+++ Foodlidays-Client/ProduitsTableVC.swift
@@ -22,47 +22,55 @@ struct Product {
 }
 
 
-class ProduitsTableVC: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+
+class ProduitsTableVC: UITableViewController, UITableViewDataSource, UITableViewDelegate{
+
+
     
     var zipCodeClient: AnyObject!
     var jsonError: NSError?
     
-    var responseArray:NSDictionary!
     var products: Product!
+    var jsonDictionary: NSArray!
     
-    lazy var data = NSMutableData()
+    @IBAction func action(sender: AnyObject) {
+        println(self.jsonDictionary)
+    }
     
     func retrieveProducts(){
         
         Alamofire.request(.GET, "http:foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/1435"
-        ).responseJSON() {
-            data in
-            println(data)
+         ).responseJSON{ (_,_,JSON,error) in
+        
+            self.jsonDictionary = JSON as NSArray
+
+            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        retrieveProducts()
         tableView.registerNib(UINib(nibName: "ProductCell", bundle: nil), forCellReuseIdentifier: "ProductCellOne")
-    
-
     }
     
     override func tableView(
         tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-            
-            return 1
+           return 1
     }
 
     override func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCell {
-            
-            var cell = tableView.dequeueReusableCellWithIdentifier("cell") as ProductCell
-                retrieveProducts()
         
-            println(responseArray)
-            
+
+        
+        
+            var cell = tableView.dequeueReusableCellWithIdentifier("cell") as ProductCell
+            println(self.jsonDictionary)
+        
             return cell
+        
     }
 
 }
