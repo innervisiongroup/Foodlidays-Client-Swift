97d0f649f52748e7fa1618263c255a71c6984099
timothyk0908 <timothyk0908@gmail.com>
2015-03-19 09:27:42 +0100

    Ajout de la liste finale, correction à effectuer sur les threads

--- Foodlidays-Client/ProduitsTableVC.swift
+++ Foodlidays-Client/ProduitsTableVC.swift
@@ -13,12 +13,16 @@ class ProductCell : UITableViewCell{
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var note: UILabel!
+    @IBOutlet weak var img: UIImageView!
     
 }
 
 struct Product {
     var label: String
     var price: String
+    var note:  String
+    var img:   UIImage
 }
 
 
@@ -37,40 +41,65 @@ class ProduitsTableVC: UITableViewController, UITableViewDataSource, UITableView
         println(self.jsonDictionary)
     }
     
+    override func viewWillAppear(animated: Bool) {
+        self.tableView .reloadData()
+    }
+    
     func retrieveProducts(){
         
         Alamofire.request(.GET, "http:foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/1435"
          ).responseJSON{ (_,_,JSON,error) in
-        
             self.jsonDictionary = JSON as NSArray
-
-            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        retrieveProducts()
+        dispatch_async(dispatch_get_main_queue()){
+        self.retrieveProducts()
+        }
         tableView.registerNib(UINib(nibName: "ProductCell", bundle: nil), forCellReuseIdentifier: "ProductCellOne")
+        refreshTable()
+
     }
     
     override func tableView(
         tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-           return 1
+           return 5
     }
 
     override func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCell {
         
-
+         var cell = tableView.dequeueReusableCellWithIdentifier("cell") as ProductCell
         
+            if(self.jsonDictionary != nil)
+            {
         
-            var cell = tableView.dequeueReusableCellWithIdentifier("cell") as ProductCell
-            println(self.jsonDictionary)
+            var rowData: NSDictionary = self.jsonDictionary[indexPath.row] as NSDictionary
+            var title=rowData["title"] as String
+            var poster=rowData["note"] as String
+            var descrip=rowData["price"] as String
+        
+            cell.label.text = title
+            cell.note.text = poster
+            cell.price.text = descrip
         
             return cell
+        }
         
+            else {println("not loaded")}
+        
+        return cell
+        
+    }
+    
+    func refreshTable()
+    {
+        dispatch_async(dispatch_get_main_queue(),{
+            self.tableView.reloadData()
+            return
+        })
     }
 
 }
